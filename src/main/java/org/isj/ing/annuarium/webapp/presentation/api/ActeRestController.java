069e6d78128feddf26c7b6c56f2e6ac8769ec3cf
FKC <batfkc@gmail.com>
2022-05-10 12:10:26 +0100

    presentation.api: ActeRestController done

---
+++ src/main/java/org/isj/ing/annuarium/webapp/presentation/api/ActeRestController.java
@@ -0,0 +1,47 @@
+package org.isj.ing.annuarium.webapp.presentation.api;
+
+import lombok.extern.slf4j.Slf4j;
+import org.isj.ing.annuarium.webapp.model.dto.ActeDto;
+import org.isj.ing.annuarium.webapp.services.IActe;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/acte")
+@Slf4j
+public class ActeRestController {
+
+	@Autowired
+	private IActe iActe;
+
+
+	@PostMapping
+	public void enregistrer(@RequestBody ActeDto create) {
+		ActeRestController.log.info("enregistrer-materiel");
+		iActe.saveActe(create);
+	}
+
+	@GetMapping("/{numero}/data")
+	public ResponseEntity<ActeDto> getActeByNumero(@PathVariable String numero){
+		return ResponseEntity.ok(iActe.searchActeByNumero(numero));
+	}
+
+
+	@GetMapping("/all")
+	public ResponseEntity<List<ActeDto>> getAllActes() {
+		return ResponseEntity.ok(iActe.listActes());
+	}
+
+	@GetMapping("/{numero}/delete")
+	public void deteleActe(@PathVariable String numero){
+		iActe.deleteActe(numero);
+	}
+
+}
