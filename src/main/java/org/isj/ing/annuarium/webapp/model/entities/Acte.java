ce1b12d3b0f1344d63a32cb28a74e76be0fe59c5
FKC <batfkc@gmail.com>
2022-05-10 09:43:42 +0100

    Acte: Entity created

--- src/main/java/org/isj/ing/annuarium/webapp/model/entities/Acte.java
+++ src/main/java/org/isj/ing/annuarium/webapp/model/entities/Acte.java
@@ -1,11 +1,20 @@
 package org.isj.ing.annuarium.webapp.model.entities;
 
 import lombok.AllArgsConstructor;
+import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
 
 @NoArgsConstructor
 @AllArgsConstructor
+@Data
+@Entity
 public class Acte {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
     private String nom;
     private String prenom;
     private String dateNaissance;
