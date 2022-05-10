bc302fe8f43a102059d07a515685709ccc0d028f
FKC <batfkc@gmail.com>
2022-05-10 09:57:21 +0100

    ActeRepository: Created

---
+++ src/main/java/org/isj/ing/annuarium/webapp/repository/ActeRepository.java
@@ -0,0 +1,7 @@
+package org.isj.ing.annuarium.webapp.repository;
+
+import org.isj.ing.annuarium.webapp.model.entities.Acte;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ActeRepository extends JpaRepository<Acte, Long> {
+}
