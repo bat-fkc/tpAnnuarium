af9cea5167cbc3cd61154a723cc3ab2e9a5712ad
FKC <batfkc@gmail.com>
2022-05-14 13:33:22 +0100

    List refactor with thymleaf

---
+++ src/main/java/org/isj/ing/annuarium/webapp/presentation/controller/ActeController.java
@@ -0,0 +1,33 @@
+package org.isj.ing.annuarium.webapp.presentation.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.isj.ing.annuarium.webapp.services.IActe;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
+
+@Controller
+@Slf4j
+public class ActeController {
+
+	@Autowired
+	IActe iActe;
+
+	@GetMapping("/")
+	public String pageAcceuil(Model model) {
+		return "index";
+	}
+
+	@GetMapping("/listactes")
+	public String pageList(Model model) {
+		//appel de la couche service
+		return "liste";
+	}
+
+}
